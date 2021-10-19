56df0f7a7f229fda6e560e815ae13681478e715f
Vedansh_yadav <83397824+Official-Vedansh-Yadav@users.noreply.github.com>
2021-10-19 10:08:05 +0530

    Delete idiscuss directory

--- idiscuss/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    session_unset();
-    session_destroy();
-    $requets_uri = $_SERVER['REQUEST_URI'];
-    header("location: $requets_uri");
-?>
--- idiscuss/partials/_footer.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    echo "
-
-        <footer class='bg-dark d-lr-flex d-md-flex d-flex w-100'>
-            <P class='text-center text-light w-100 mt-1'>Copyright &copy; 2021 @Vedanshphpforum.com - All rights are claimed</p>
-        </footer>
-    ";
-?>
